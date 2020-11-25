08bc91ee88642330470b2be864b5ff88810bb4bd
nssina <rabie_sina@yahoo.com>
2020-11-25 13:29:49 -0500

    Inital commit

---
+++ Nobitex/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nobitex
+//
+//  Created by Sina Rabiei on 11/25/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
